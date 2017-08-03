2e69ac65350c9c9bb557a3a98df0e41ac8544ed9
luckyxxl <mail@luckyxxl.de>
2017-08-03 07:57:55 +0200

    Add glm

--- source/main.hpp
+++ source/main.hpp
@@ -13,14 +13,16 @@
 #include <tuple>
 #include <vector>
 
+#include <glm/glm.hpp>
+
+#include <json.hpp>
+using json = nlohmann::json;
+
 #include <SDL.h>
 #define GL_GLEXT_PROTOTYPES
 #include <SDL_opengl.h>
 
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-
-#include <json.hpp>
-using json = nlohmann::json;
 
 constexpr auto PI = 3.14159265f;
