ad24f11b35988c29587b3153a6636bcce4c48376
luckyxxl <mail@luckyxxl.de>
2017-08-03 07:50:44 +0200

    Add 3rdparty directory and move json.hpp in there

--- source/main.hpp
+++ source/main.hpp
@@ -20,7 +20,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
-#include "json.hpp"
+#include <json.hpp>
 using json = nlohmann::json;
 
 constexpr auto PI = 3.14159265f;
