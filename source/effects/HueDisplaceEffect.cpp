3932fb8a0cca0ae18d3f3dd12ec2421b7838d84d
luckyxxl <mail@luckyxxl.de>
2017-08-07 10:40:15 +0200

    Always register HueDisplace effect, independent of distance config

--- source/effects/HueDisplaceEffect.cpp
+++ source/effects/HueDisplaceEffect.cpp
@@ -32,52 +32,50 @@ void HueDisplaceEffect::randomizeConfig() {}
 void HueDisplaceEffect::registerEffect(Uniforms &uniforms,
                                        ShaderBuilder &vertexShader,
                                        ShaderBuilder &fragmentShader) const {
-  if (distance != 0.f) {
-    vertexShader.appendMainBody(
-        TEMPLATE(R"glsl(
-    {
-      float angle = hsv[0] + ${directionOffset};
-      float offset = (-cos(${time}) + 1.) / 2.;
-      position.xy += offset * getDirectionVector(angle) * ${distance} * (1. - ${scaleByVal} * (1. - hsv[2])) * (1. - ${scaleByFg} * (1. - foregroundMask));
-    }
-    )glsl")
-            .compile({
-                UNIFORM("distance", GLSLType::Float,
-                        [this](const RenderProps &props) {
-                          return UniformValue(distance);
-                        }),
-                UNIFORM("time", GLSLType::Float,
-                        [this](const RenderProps &props) {
-                          return UniformValue(
-                              (props.state.clock.getTime() - timeBegin) /
-                              getPeriod() * 2 * PI);
-                        }),
-                UNIFORM("directionOffset", GLSLType::Float,
-                        [this](const RenderProps &props) {
-                          auto result =
-                              rotate *
-                              (props.state.clock.getTime() - timeBegin) /
-                              getPeriod() * 2 * PI;
-                          if (randomDirectionOffset) {
-                            if (std::isnan(randomDirectionOffsetValue)) {
-                              std::uniform_real_distribution<float> dist;
-                              const_cast<HueDisplaceEffect *>(this)
-                                  ->randomDirectionOffsetValue =
-                                  dist(props.random) * 2 * PI;
-                            }
-                            result += randomDirectionOffsetValue;
-                          }
-                          return UniformValue(result);
-                        }),
-                UNIFORM("scaleByVal", GLSLType::Float,
-                        [this](const RenderProps &props) {
-                          return UniformValue(scaleByValue);
-                        }),
-                UNIFORM("scaleByFg", GLSLType::Float,
-                        [this](const RenderProps &props) {
-                          return UniformValue(scaleByForegroundMask);
-                        }),
-            })
-            .c_str());
+  vertexShader.appendMainBody(
+      TEMPLATE(R"glsl(
+  {
+    float angle = hsv[0] + ${directionOffset};
+    float offset = (-cos(${time}) + 1.) / 2.;
+    position.xy += offset * getDirectionVector(angle) * ${distance} * (1. - ${scaleByVal} * (1. - hsv[2])) * (1. - ${scaleByFg} * (1. - foregroundMask));
   }
+  )glsl")
+          .compile({
+              UNIFORM("distance", GLSLType::Float,
+                      [this](const RenderProps &props) {
+                        return UniformValue(distance);
+                      }),
+              UNIFORM("time", GLSLType::Float,
+                      [this](const RenderProps &props) {
+                        return UniformValue(
+                            (props.state.clock.getTime() - timeBegin) /
+                            getPeriod() * 2 * PI);
+                      }),
+              UNIFORM("directionOffset", GLSLType::Float,
+                      [this](const RenderProps &props) {
+                        auto result =
+                            rotate *
+                            (props.state.clock.getTime() - timeBegin) /
+                            getPeriod() * 2 * PI;
+                        if (randomDirectionOffset) {
+                          if (std::isnan(randomDirectionOffsetValue)) {
+                            std::uniform_real_distribution<float> dist;
+                            const_cast<HueDisplaceEffect *>(this)
+                                ->randomDirectionOffsetValue =
+                                dist(props.random) * 2 * PI;
+                          }
+                          result += randomDirectionOffsetValue;
+                        }
+                        return UniformValue(result);
+                      }),
+              UNIFORM("scaleByVal", GLSLType::Float,
+                      [this](const RenderProps &props) {
+                        return UniformValue(scaleByValue);
+                      }),
+              UNIFORM("scaleByFg", GLSLType::Float,
+                      [this](const RenderProps &props) {
+                        return UniformValue(scaleByForegroundMask);
+                      }),
+          })
+          .c_str());
 }
