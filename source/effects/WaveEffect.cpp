luckyxxl/hfg-webcam-particles

commit 1ff72b482054e499e9557e471bfb4c5070ac5f62

Make every parameter an uniform, so we can change them without having to rebuild the pipeline

source/effects/WaveEffect.cpp
@@ -35,7 +35,7 @@ void WaveEffect::registerEffect(Uniforms &uniforms,
     float x = 2 * ${time} - initialPosition.x;
 
     float ease = 1.;
-    if ((${rep} == 0 && x <= 0.5) || (${rep} == ${instance.repetitions} - 1 && x >= 0.5)) {
+    if ((${rep} == 0 && x <= 0.5) || (${rep} == ${repetitions} - 1 && x >= 0.5)) {
       // The ease function is a cos spanning two negative peaks with a positive peak
       // in between. This is is then translated (+1, /2) to go from 0 to 1
       // Finally, because this will lower the actual peak height of `curve`
@@ -79,7 +79,10 @@ void WaveEffect::registerEffect(Uniforms &uniforms,
                       [this](const RenderProps &props) {
                         return UniformValue(amplitude);
                       }),
-              { "instance.repetitions", std::to_string(repetitions) },
+              UNIFORM("repetitions", GLSLType::Float,
+                      [this](const RenderProps &props) {
+                        return UniformValue(repetitions);
+                      }),
           })
           .c_str());
 }

source/ParticleRenderer.cpp
@@ -99,9 +99,20 @@ void ParticleRenderer::setTimeline(std::unique_ptr<Timeline> _timeline) {
   unsigned instanceId = 0;
   timeline->forEachInstance([&](const IEffect &i) {
     Uniforms instanceUniforms(uniforms, instanceId++);
-    vertexShader.appendMainBody(("if(" + std::to_string(i.getTimeBegin()) +
-                                 " <= globalTime && globalTime <= " +
-                                 std::to_string(i.getTimeEnd()) + ") {")
+    const auto timeBegin = instanceUniforms.addUniform("timeBegin",
+      GLSLType::Float, [&i](const RenderProps &props) {
+        return UniformValue(i.timeBegin);
+      });
+    const auto timeEnd = instanceUniforms.addUniform("timeEnd",
+      GLSLType::Float, [&i](const RenderProps &props) {
+        return UniformValue(i.timeEnd);
+      });
+    vertexShader.appendMainBody(TEMPLATE("if(${timeBegin} <= globalTime && "
+                                         "globalTime <= ${timeEnd}) { ")
+                                         .compile({
+                                           {"timeBegin", timeBegin},
+                                           {"timeEnd", timeEnd},
+                                         })
                                     .c_str());
 #if 1
     vertexShader.appendMainBody("\n#line 0\n");

commit ed8a57a6703e0d2290fcc2ea5d8592500c4d243d

Fix broken Wave effect introduced in 1ff72b482054e499e9557e471bfb4c5070ac5f62

source/effects/WaveEffect.cpp
@@ -51,7 +51,7 @@ void WaveEffect::registerEffect(Uniforms &uniforms,
       reached = 1.;
     }
     float notOver = (x <= 1.) ? 1. : 0.;
-    if (${rep} != ${instance.repetitions} - 1) {
+    if (${rep} != ${repetitions} - 1) {
       notOver = 1.;
     }
 