26450b88b912ea2f6c0d88bd0d83b160bf41835a
Lukas Böhm <suluke93@gmail.com>
2017-09-03 18:39:36 +0200

    Add Resources::resolve method

--- source/Resources.hpp
+++ source/Resources.hpp
@@ -1,15 +1,19 @@
 #pragma once
+#include <SDL.h>
 
 class Resources {
 public:
+  using path_t = std::string;
+
   bool create(const char *argv0);
   void destroy();
 
+  path_t resolve(const char *filename);
   SDL_RWops *openFile(const char *filename);
 
   std::string readWholeTextFile(const char *filename);
   std::vector<uint8_t> readWholeBinaryFile(const char *filename);
 
 private:
-  std::string rootPath;
+  path_t rootPath;
 };
